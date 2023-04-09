d21cc3818fee9b403a53219f9b1a3851d6db3589
LeAnhTuan647 <92526269+AnhTuan0011@users.noreply.github.com>
2023-04-10 00:00:25 +0700

    2703

--- BTH2703/Migrations/20230409165533_Create_Table_Employee.Designer.cs
+++ BTH2703/Migrations/20230409165533_Create_Table_Employee.Designer.cs
@@ -2,6 +2,7 @@
 using BTH2703.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BTH2703.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230409165533_Create_Table_Employee")]
+    partial class Create_Table_Employee
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.4");
